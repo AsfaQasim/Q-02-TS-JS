68b48f7270dd2df05ee83afe2869909d133c99e7
Asfa Qasim <160766420+AsfaQasim@users.noreply.github.com>
2024-03-07 17:03:02 -0800

    Q 02 Ts/js & node js of 45 assignment
    Q 02 Ts/js & node js of 45 assignment

---
+++ main.js
@@ -0,0 +1,2 @@
+var personName = "ERIC";
+console.log("hello ".concat(personName, " would you like to learn some Python today?"));
---
+++ main.ts
@@ -0,0 +1,2 @@
+let personName: string= "ERIC";
+console.log(`hello ${personName} would you like to learn some Python today?`);
